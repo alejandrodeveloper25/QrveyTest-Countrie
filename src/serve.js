30a01ae26fd0f8af2a631a71c471d91a6ebf6471
alejandrodeveloper25 <alejandro.gonz.jimz@gmail.com>
2020-09-08 13:52:49 -0500

    Update serve.js

--- src/serve.js
+++ src/serve.js
@@ -1,8 +1,8 @@
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/qrvey-test'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/qrvey-test/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
