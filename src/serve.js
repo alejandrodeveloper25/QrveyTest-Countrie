b206edd57aff9dd6b3515ae63cb35bc14805f798
alejandrodeveloper25 <alejandro.gonz.jimz@gmail.com>
2020-09-08 14:40:21 -0500

    Update serve.js

--- src/serve.js
+++ src/serve.js
@@ -1,8 +1,8 @@
 const app = express();
 
-app.use(express.static(__dirname+'/dist/qrvey-test'));
+app.use(express.static(__dirname+'/src/qrvey-test'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/qrvey-test/index.html'));
+    res.sendFile(path.join(__dirname+'/src/qrvey-test/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
